25624300ecfef13632560ed4797d16e5ea3812a2
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-04-30 11:42:34 +0000

    aggiunto passaggio dati tra componenti + ricordare creazione server

--- mapboxProva/src/app/map.service.ts
+++ mapboxProva/src/app/map.service.ts
@@ -4,18 +4,25 @@ import { environment } from "../environments/environment";
 
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
+  dataSubject : Subject<any> = new Subject<any>();
   map: mapboxgl.Map;
   geocoder: MapboxGeocoder;
   style = 'mapbox://styles/mapbox/streets-v11';
   lat = 45.4628328;
   lng = 9.1076927;
   zoom = 12
+
+  getDataSubjectObs() : Observable<any>
+  {
+    return this.dataSubject.asObservable();
+  }
 
   constructor() {
     mapboxgl.accessToken = environment.mapbox.accessToken;
@@ -38,9 +45,10 @@ export class MapService {
     );
 
     //restituisce il json della ricerca
-    this.geocoder.on('result', function (ev) {
+    this.geocoder.on('result', (ev) => {
       var styleSpec = ev.result;
       console.log(styleSpec);
+      this.dataSubject.next(styleSpec);
     });
 
     //per aumentare, diminuire, ruotare la mappa
--- mapboxProva/src/app/map/map.component.ts
+++ mapboxProva/src/app/map/map.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from '../map.service';
 
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  results: any;
 
   constructor(private map: MapService) { }
 
-
   ngOnInit() {
-    this.map.buildMap()
+    this.map.buildMap();
+
+    this.map.getDataSubjectObs().subscribe(message => this.results = message);
   }
 
 }
