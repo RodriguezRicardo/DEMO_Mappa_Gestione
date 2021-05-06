73c95b70e06f55b49f5bac1cb0f4278f40d1c173
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-05-06 20:23:43 +0000

    some fix + updated graphic

--- mapboxProva/src/app/places.service.ts
+++ mapboxProva/src/app/places.service.ts
@@ -8,7 +8,7 @@ export class PlacesService {
 
   constructor(private http: HttpClient) { }
   searchPlace(y: any, x: any, y1 :any, x1: any) {
-    const url = `https://3000-turquoise-flyingfish-srj91d7n.ws-eu03.gitpod.io/place/${y}/${x}/${y1}/${x1}`;
+    const url = `https://3000-maroon-limpet-mmuu0k3f.ws-eu05.gitpod.io/place/${y}/${x}/${y1}/${x1}`;
     return this.http.get(url);
   }
 }
